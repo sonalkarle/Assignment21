f37b29d60c7577343fcc15e29d89b9df8b4516c8
sonal <sonalkarle01@gmail.com>
2020-12-26 00:03:06 +0530

    Solving using switvh case

--- employeeWage.java
+++ employeeWage.java
@@ -1,32 +1,34 @@
-class employeeWage
+public class employeewage
 {
-	public static void main(String[] args)
-	{
-		
-                 int isfulltime=1;
-                int isparttime=2;
-                int empRatePerHr=20;
+        public static final int FULL_TIME=1;
+        public static final int PART_TIME=2;
+        public static final int EMP_RATE_PER_HR=20;
+
+
+        public static void main(String[] args)
+        {
+                //variable
                 int empHr=0;
                 int salary=0;
-                int res=(int)(Math.random()*10)%3;
-                if(res==isfulltime)
-                {
+                //Computation
+                int empCheck=(int)Math.floor(Math.random()*10)%3;
+                switch (empCheck) {
+
+                        case FULL_TIME:
                         System.out.println("Employee is fulltime");
                         empHr=8;
-                }
-                else if(res==isparttime)
-                {
-                        System.out.println("Employee is Parttime");
+                        break;
+			 case PART_TIME:
+                        System.out.println("Employee is parttime");
                         empHr=4;
-		 }
-                else
-                {
+                        break;
+                        default:
                         System.out.println("Employee is absent");
                         empHr=0;
+
                 }
-                salary=(empHr*empRatePerHr);
+                salary=(empHr*EMP_RATE_PER_HR);
                 System.out.println("Salary is "+salary);
-
-
-	}
+        }
 }
+
