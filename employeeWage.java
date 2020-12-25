62d72348d5e36ade8d15bed524d3f16312467ab9
sonal <sonalkarle01@gmail.com>
2020-12-26 00:14:39 +0530

    Calculate the wages till final hour or days reached

--- employeeWage.java
+++ employeeWage.java
@@ -9,24 +9,47 @@ class employeeWage
                 int empHr=0;
                 int salary=0;
                 int res=(int)(Math.random()*10)%3;
-                if(res==isfulltime)
+      
+public class employeewage
+{
+        public static final int FULL_TIME=1;
+        public static final int PART_TIME=2;
+        public static final int EMP_RATE_PER_HR=20;
+        public static final int NUM_OF_WORKINGDAYS=2;
+        public static final int MAX_HRS_IN_MONTH=10;
+
+        public static void main(String[] args)
+        {
+                int empHr=0;
+                int totalEmpHr=0;
+                int totalWorkingDays=0;
+                while (totalEmpHr<=MAX_HRS_IN_MONTH && totalWorkingDays<=NUM_OF_WORKINGDAYS)
                 {
+                        totalWorkingDays++;
+                        int empCheck=(int) Math.floor(Math.random()*10)%3;
+                        switch (empCheck)
+                        {
+                        case FULL_TIME:
                         System.out.println("Employee is fulltime");
                         empHr=8;
-                }
-                else if(res==isparttime)
-                {
-                        System.out.println("Employee is Parttime");
+                        break;
+                        case PART_TIME:
+                        System.out.println("Employee is parttime");
                         empHr=4;
-		 }
-                else
-                {
+                        break;
+                        default:
                         System.out.println("Employee is absent");
                         empHr=0;
+                        }
+                totalEmpHr += empHr;
+
                 }
-                salary=(empHr*empRatePerHr);
-                System.out.println("Salary is "+salary);
+                int totalsalary = totalEmpHr * EMP_RATE_PER_HR;
+                System.out.println("Total salary: "+totalsalary);
 
 
-	}
+        }
 }
+
+
+
