cb7596dc6ce2f6d5e0ad501be77bcca75a650218
sonalkarle <74963220+sonalkarle@users.noreply.github.com>
2021-01-02 09:58:39 +0530

    Update employeeWage.java

--- employeeWage.java
+++ employeeWage.java
@@ -2,13 +2,15 @@ class employeeWage
 {
 	public static void main(String[] args)
 	{
-		
+		//variables
                 int isPresent=1;
                 int empRatePerHr=20;
                 int salary=0;
                 int empHr=0;
+		//computation
                 int res=(int)(Math.random()*10)%2;
-                if(res==isPresent)
+                //condition
+		if(res==isPresent)
                 {
                         System.out.println("Employee is present");
                         empHr=8;
@@ -18,6 +20,7 @@ class employeeWage
                         System.out.println("Employee is absent");
                         empHr=0;
                 }
+		//final answer
                 salary=(empRatePerHr*empHr);
 		System.out.println("Salary is " +salary);		
 
