90e7c0d9508759a220faadaa2ab3eb0cdaa9e6b6
sonalkarle <74963220+sonalkarle@users.noreply.github.com>
2021-01-02 10:00:19 +0530

    Update employeeWage.java

--- employeeWage.java
+++ employeeWage.java
@@ -1,5 +1,6 @@
 public class employeeWage
 {
+	//constant
         public static final int FULL_TIME=1;
         public static final int PART_TIME=2;
         public static final int EMP_RATE_PER_HR=20;
@@ -12,6 +13,7 @@ public class employeeWage
                 int salary=0;
                 //Computation
                 int empCheck=(int)Math.floor(Math.random()*10)%3;
+		//condition
                 switch (empCheck) {
 
                         case FULL_TIME:
