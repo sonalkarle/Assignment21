a7732e5877c3d3c544282d42c3c0261189e29ef7
sonalkarle <74963220+sonalkarle@users.noreply.github.com>
2021-01-02 10:08:26 +0530

    Update employeeWage.java

--- employeeWage.java
+++ employeeWage.java
@@ -1,5 +1,6 @@
 public class employeeWage
 {
+	//Refactor the code for calculating monthly wage of fulltime & parttime employee
         public static final int FULL_TIME=1;
         public static final int PART_TIME=2;
         public static final int EMP_RATE_PER_HR=20;
