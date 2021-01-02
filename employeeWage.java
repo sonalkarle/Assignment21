18895da98ef2c22cedfb970e06fb227903681cda
sonalkarle <74963220+sonalkarle@users.noreply.github.com>
2021-01-02 09:59:32 +0530

    Update employeeWage.java

--- employeeWage.java
+++ employeeWage.java
@@ -3,12 +3,16 @@ class employeeWage
 	public static void main(String[] args)
 	{
 		
-                 int isfulltime=1;
+	
+		//variables
+                int isfulltime=1;
                 int isparttime=2;
                 int empRatePerHr=20;
                 int empHr=0;
                 int salary=0;
+		//computation
                 int res=(int)(Math.random()*10)%3;
+		//condition
                 if(res==isfulltime)
                 {
                         System.out.println("Employee is fulltime");
